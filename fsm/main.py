1c63523e71d6f9f0b039e5e7e511c792213265bd
Yerzhan Aitzhanov <Yerzhan.Aitzhanov@erg.kz>
2020-10-20 13:23:48 +0600

    added more examples

---
+++ fsm/main.py
@@ -0,0 +1,120 @@
+"""
+Finite State Machine (Конечный автомат).
+
+https://ru.wikipedia.org/wiki/Конечный_автомат
+
+Смартчас "Telegram Chat Bot Programming (advanced)"
+"""
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+logging.basicConfig(level=logging.INFO)
+
+# скопируйте токен полученный от @BotFather
+BOT_TOKEN = ''
+
+storage = MemoryStorage()
+
+# Инициализация бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot, storage=storage)
+
+# список состояний
+# доп. пример смотрите также https://github.com/aiogram/aiogram/blob/1e2fe72aca12a3fc6f2d1f66c71539af5a84ea00/examples/finite_state_machine_example.py
+CHOOSE_MENU = 'choose_menu'
+CHOSEN_INLINE_MENU = 'chosen_inline_menu'
+CHOSEN_REPLY_MENU = 'chosen_reply_menu'
+
+
+# основное меню
+MAIN_MENU_KEYBOARD = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+MAIN_MENU_KEYBOARD.add(types.KeyboardButton('Обычные кнопки'))
+MAIN_MENU_KEYBOARD.add(types.KeyboardButton('Инлайн кнопки'))
+
+
+REPLY_KEYBOARD_MENU = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+REPLY_KEYBOARD_MENU.add(types.KeyboardButton('1'))
+REPLY_KEYBOARD_MENU.add(types.KeyboardButton('2'))
+REPLY_KEYBOARD_MENU.add(types.KeyboardButton('3'))
+
+
+INLINE_KEYBOARD_MENU = types.InlineKeyboardMarkup()
+INLINE_KEYBOARD_MENU.add(types.InlineKeyboardButton('Первая кнопка!', callback_data='button1'))
+INLINE_KEYBOARD_MENU.add(types.InlineKeyboardButton('Вторая кнопка!', callback_data='button2'))
+INLINE_KEYBOARD_MENU.add(types.InlineKeyboardButton('Третья кнопка!', callback_data='button3'))
+
+
+CANCEL_MENU = types.ReplyKeyboardMarkup()
+CANCEL_MENU.add(types.KeyboardButton('Отмена'))
+
+
+@dp.message_handler(state='*', commands=['start'])
+@dp.message_handler(state='*', regexp=r'^Отмена$')
+async def send_menus(message: types.Message):
+    state = dp.current_state(chat=message.chat.id, user=message.chat.id)
+    await state.set_state(CHOOSE_MENU)
+    current_state = await state.get_state()
+    text = "Пример конечного автомата, веберите меню (состояние: {})".format(current_state)
+    await message.reply(text, reply_markup=MAIN_MENU_KEYBOARD)
+
+
+@dp.message_handler(state=CHOOSE_MENU, regexp=r'^Обычные кнопки$')
+async def reply_keyboard_menu_handler(message: types.Message):
+    state = dp.current_state(chat=message.chat.id, user=message.chat.id)
+    await state.set_state(CHOSEN_REPLY_MENU)
+    current_state = await state.get_state()
+    text = 'Вы выбрали меню: "{}" (состояние {})'.format(message.text, current_state)
+    await bot.send_message(message.chat.id, text, reply_markup=REPLY_KEYBOARD_MENU)
+
+
+@dp.message_handler(state=CHOOSE_MENU, regexp=r'^Инлайн кнопки$')
+async def inline_keyboard_menu_handler(message: types.Message):
+    state = dp.current_state(chat=message.chat.id, user=message.chat.id)
+    await state.set_state(CHOSEN_INLINE_MENU)
+    current_state = await state.get_state()
+    text = 'Вы выбрали меню: "{}" (состояние: {})'.format(message.text, current_state)
+    await bot.send_message(message.chat.id, text, reply_markup=INLINE_KEYBOARD_MENU)
+
+
+@dp.message_handler(state=CHOSEN_REPLY_MENU)
+async def reply_messages_handler(message: types.Message):
+    state = dp.current_state(chat=message.chat.id, user=message.chat.id)
+    current_state = await state.get_state()
+    await bot.send_message(message.chat.id, "Нажата кнопка: {} (состояние: {})".format(message.text, current_state))
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button1', state=CHOSEN_INLINE_MENU)
+async def inline_callback_button_1(callback: types.CallbackQuery):
+    state = dp.current_state(chat=callback.from_user.id, user=callback.from_user.id)
+    await state.set_state(CHOSEN_INLINE_MENU)
+    current_state = await state.get_state()
+    await bot.send_message(callback.from_user.id, "Нажата кнопка: 1 (состояние: {})".format(current_state))
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button2', state=CHOSEN_INLINE_MENU)
+async def inline_callback_button_2(callback: types.CallbackQuery):
+    state = dp.current_state(chat=callback.from_user.id, user=callback.from_user.id)
+    await state.set_state(CHOSEN_INLINE_MENU)
+    current_state = await state.get_state()
+    await bot.send_message(callback.from_user.id, "Нажата кнопка: 2 (состояние: {})".format(current_state))
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button3', state=CHOSEN_INLINE_MENU)
+async def inline_callback_button_3(callback: types.CallbackQuery):
+    state = dp.current_state(chat=callback.from_user.id, user=callback.from_user.id)
+    await state.set_state(CHOSEN_INLINE_MENU)
+    current_state = await state.get_state()
+    await bot.send_message(callback.from_user.id, "Нажата кнопка: 3 (состояние: {})".format(current_state))
+
+
+@dp.message_handler(state='*')
+async def unknown_message_handler(message: types.Message):
+    state = dp.current_state(chat=message.chat.id, user=message.chat.id)
+    current_state = await state.get_state()
+    await bot.send_message(message.from_user.id, "Не понял Ваше сообщение (состояние: {})".format(current_state))
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ throttling_and_logging/main.py
@@ -0,0 +1,119 @@
+"""
+Пример троттлинга ("антифлуд").
+
+Смартчас "Telegram Chat Bot Programming (advanced)"
+"""
+
+import logging
+import asyncio
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import DEFAULT_RATE_LIMIT
+from aiogram.dispatcher.handler import CancelHandler, current_handler
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.exceptions import Throttled
+
+# скопируйте токен полученный от @BotFather
+BOT_TOKEN = ''
+
+# логгирование
+logging.basicConfig(level=logging.DEBUG)
+
+storage = MemoryStorage()
+
+# Инициализация бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot, storage=storage)
+
+
+async def on_shutdown(disp):
+    await disp.storage.close()
+    await disp.storage.wait_closed()
+
+
+class ThrottlingMiddleware(BaseMiddleware):
+    """
+    Simple middleware
+    """
+
+    # rate limit можно подкрутить
+    # def __init__(self, limit=1.5, key_prefix='antiflood_'):
+    def __init__(self, limit=DEFAULT_RATE_LIMIT, key_prefix='antiflood_'):
+        self.rate_limit = limit
+        self.prefix = key_prefix
+        super(ThrottlingMiddleware, self).__init__()
+
+    async def on_process_message(self, message: types.Message, data: dict):
+        """
+        This handler is called when dispatcher receives a message
+        :param message:
+        """
+        # Get current handler
+        handler = current_handler.get()
+
+        # Get dispatcher from context
+        dispatcher = Dispatcher.get_current()
+        # If handler was configured, get rate limit and key from handler
+        if handler:
+            limit = getattr(handler, 'throttling_rate_limit', self.rate_limit)
+            key = getattr(handler, 'throttling_key', f"{self.prefix}_{handler.__name__}")
+        else:
+            limit = self.rate_limit
+            key = f"{self.prefix}_message"
+
+        # Use Dispatcher.throttle method.
+        try:
+            await dispatcher.throttle(key, rate=limit)
+        except Throttled as t:
+            # Execute action
+            await self.message_throttled(message, t)
+
+            # Cancel current handler
+            raise CancelHandler()
+
+    async def message_throttled(self, message: types.Message, throttled: Throttled):
+        """
+        Notify user only on first exceed and notify about unlocking only on last exceed
+        :param message:
+        :param throttled:
+        """
+        # Calculate how many time is left till the block ends
+        delta = throttled.rate - throttled.delta
+
+        # Prevent flooding
+        if throttled.exceeded_count <= 2:
+            await message.reply('Too many requests! ')
+
+        # Sleep.
+        await asyncio.sleep(delta)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    """
+    Оператор, отслеживающий событие: пользователь ввел `/start`
+    """
+    text = "Пример антифлуда"  # можно поменять текст
+    keyboard_markup = types.ReplyKeyboardMarkup()
+
+    keyboard_markup.add(types.KeyboardButton('flood'))
+
+    await message.reply(text, reply_markup=keyboard_markup)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    """
+    Простой оператор, который отправляет введенный раннее юзером текст
+    """
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    # Setup middleware
+    dp.middleware.setup(ThrottlingMiddleware())
+    # dp.middleware.setup(LoggingMiddleware())
+
+    executor.start_polling(dp, skip_updates=True, on_shutdown=on_shutdown)
